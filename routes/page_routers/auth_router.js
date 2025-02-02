6824244428c9392cea781fc973bb1116fe0d52bd
JouSee11 <pepa.talac@gmail.com>
2025-02-02 16:20:11 +0100

    registration

--- routes/page_routers/auth_router.js
+++ routes/page_routers/auth_router.js
@@ -1,5 +1,8 @@
 import express from "express"
 import { getLoginPage } from "../../controllers/login_controller.js"
+import { getRegisterPage } from "../../controllers/register_controller.js"
+import { handleRegister } from "../../controllers/register_controller.js"
+
 
 const router = express.Router()
 
@@ -8,5 +11,8 @@ router.use(express.static("./public"))
 router.route("/login")
 .get(getLoginPage)
 
+router.route("/register")
+.get(getRegisterPage)
+.post(handleRegister)
 
 export default router
