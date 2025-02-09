a4b2d5f8be229909342db2766e6bbe8e278ae947
Josef <pepa.talac@gmail.com>
2025-02-09 16:14:39 +0100

    Add redirect for unauthenticated users in profile controller

--- controllers/profile_controller.js
+++ controllers/profile_controller.js
@@ -5,6 +5,11 @@ import User from "../models/user_model.js"
 const getProfilePage = async (req, res) => {
     const profilePage = new ProfilePage()
 
+    //if user is not logged in
+    if(!req.session.userId) {
+        return res.redirect("/auth/login")
+    }
+
     const userObject = await User.findById(req.session.userId)
     const username = userObject.profile.username
 
