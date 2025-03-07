2705ba491c1b12b39b5fb51475531bbff59d22ec
JouSee11 <pepa.talac@gmail.com>
2025-03-07 22:56:41 +0100

    adding functionality for three pages in the app

--- public/js/home_binding/page_selection_binding.js
+++ public/js/home_binding/page_selection_binding.js
@@ -1,16 +1,38 @@
+// page buttons
 const pageOneBtn = document.getElementById('page-one');
 const pageTwoBtn = document.getElementById('page-two');
 const pageThreeBtn = document.getElementById('page-three');
 
+// page containers
+const pageOne = document.querySelector("[data-page='1']");
+const pageTwo = document.querySelector("[data-page='2']");
+const pageThree = document.querySelector("[data-page='3']");
+
 pageOneBtn.addEventListener('click', () => {
+    changeBtnPage(pageOne)
     pageBtnChangeColor(pageOneBtn);
 })
 
 pageTwoBtn.addEventListener('click', () => {
+    changeBtnPage(pageTwo)
     pageBtnChangeColor(pageTwoBtn);
 }) 
 
 pageThreeBtn.addEventListener('click', () => {
+    changeBtnPage(pageThree)
     pageBtnChangeColor(pageThreeBtn);
 })
 
+function changeBtnPage(pageButtonsCont){
+    pageOne.classList.remove("active")
+    pageTwo.classList.remove("active")
+    pageThree.classList.remove("active")
+
+    pageButtonsCont.classList.add("active")
+
+    pageButtonsCont.classList.add("showed")
+    setTimeout(() => {
+        pageButtonsCont.classList.remove("showed")
+    }, 1)
+
+}
