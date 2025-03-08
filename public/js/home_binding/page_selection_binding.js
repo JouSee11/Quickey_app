627098d459af38f5206b2f75327c8936dac65f37
Josef <pepa.talac@gmail.com>
2025-03-08 11:09:32 +0100

    feat: add keyboard navigation for page selection and refactor button activation logic

--- public/js/home_binding/page_selection_binding.js
+++ public/js/home_binding/page_selection_binding.js
@@ -8,6 +8,20 @@ const pageOne = document.querySelector("[data-page='1']");
 const pageTwo = document.querySelector("[data-page='2']");
 const pageThree = document.querySelector("[data-page='3']");
 
+
+function changeBtnPage(pageButtonsCont){
+    pageOne.classList.remove("active")
+    pageTwo.classList.remove("active")
+    pageThree.classList.remove("active")
+
+    pageButtonsCont.classList.add("active")
+
+    pageButtonsCont.classList.add("showed")
+    setTimeout(() => {
+        pageButtonsCont.classList.remove("showed")
+    }, 10)
+}
+
 pageOneBtn.addEventListener('click', () => {
     changeBtnPage(pageOne)
     pageBtnChangeColor(pageOneBtn);
@@ -23,16 +37,21 @@ pageThreeBtn.addEventListener('click', () => {
     pageBtnChangeColor(pageThreeBtn);
 })
 
-function changeBtnPage(pageButtonsCont){
-    pageOne.classList.remove("active")
-    pageTwo.classList.remove("active")
-    pageThree.classList.remove("active")
 
-    pageButtonsCont.classList.add("active")
 
-    pageButtonsCont.classList.add("showed")
-    setTimeout(() => {
-        pageButtonsCont.classList.remove("showed")
-    }, 1)
-
-}
+//change page on tab press
+document.addEventListener('keydown', (e) => {
+    if(e.key === "Tab") {
+        e.preventDefault();
+        if(pageOne.classList.contains("active")){
+            changeBtnPage(pageTwo)
+            pageBtnChangeColor(pageTwoBtn);
+        } else if(pageTwo.classList.contains("active")){
+            changeBtnPage(pageThree)
+            pageBtnChangeColor(pageThreeBtn);
+        } else if(pageThree.classList.contains("active")){
+            changeBtnPage(pageOne)
+            pageBtnChangeColor(pageOneBtn);
+        }
+    }
+})
