b622ffc5c9323aa6ef858e8ad1a0718833c4f190
JouSee11 <pepa.talac@gmail.com>
2025-03-11 21:56:00 +0100

    adding session data to mongodb collection instead of in memory

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import router from "./routes/router.js"
 import connectDB from "./db/connect.js"
 import session from "express-session"
 import passport from "passport"
+import MongoStore from "connect-mongo"
 
 
 // import keysRouter from "./routes/key-bindings.js"
@@ -21,6 +22,8 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true,
+    store: MongoStore.create({mongoUrl: process.env.MONGO_URI}),
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 1 day
 }))
 
 app.use(passport.initialize())
